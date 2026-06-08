20a49e86b9f20fa6dc1372e925cee986940d65dc
Federico Fasani <fedefasani@gmail.com>
2026-06-08 12:07:52 -0300

    Enable CORS for Angular frontend

--- src/main/java/com/fefe/backend/controller/AppointmentController.java
+++ src/main/java/com/fefe/backend/controller/AppointmentController.java
@@ -14,6 +14,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/appointments")
+@CrossOrigin(origins = "http://localhost:4200")
 @RequiredArgsConstructor
 public class AppointmentController {
 
